3ac57a2b166ffb43e2fade2ac4255c611fa9704d
Bruno L Nogueira <nugobruno@gmail.com>
2020-08-27 18:53:24 -0300

    Exercicio 01 | operadores basicos

---
+++ exercises/4.1/01_exercicio.js
@@ -0,0 +1,8 @@
+let a=10;
+let b=5;
+
+console.log('O resultado da adição é: '); console.log(a+b);
+console.log('O resultado da subtração é: '); console.log(a-b);
+console.log('O resultado da multiplicação é: '); console.log(a*b);
+console.log('O resultado da divisão é: '); console.log(a/b);
+console.log('O resultado do módulo é: '); console.log(a%b);
