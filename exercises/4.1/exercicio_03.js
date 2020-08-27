20c1794b673001ce1597f5e44403ff3a2ccea276
Bruno L Nogueira <nugobruno@gmail.com>
2020-08-27 19:05:55 -0300

    Exercicio 3 | Qual eh o maior numero

---
+++ exercises/4.1/exercicio_03.js
@@ -0,0 +1,10 @@
+let a=1000;
+let b=2000000;
+let c=10;
+
+if (a>b && a>c) {
+  console.log(a + " é o maior número");
+} else if (b>a && b>c) {
+  console.log(b + " é o maior número");
+} else
+  console.log(c + " é o maior número");
