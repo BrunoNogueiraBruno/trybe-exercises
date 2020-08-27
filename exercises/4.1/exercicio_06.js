b4812ce8c052549b424167886ffa0ca2f2e1e54f
Bruno L Nogueira <nugobruno@gmail.com>
2020-08-27 19:36:08 -0300

    Exercicio 6 | Movimento das peças de xadrez

---
+++ exercises/4.1/exercicio_06.js
@@ -0,0 +1,23 @@
+let piece = "Rei";
+let pieceLowerCase = piece.toLowerCase();
+
+switch(pieceLowerCase){
+  case 'peão':
+    console.log("O peão só pode andar uma casa a sua frente, horizontalmente");
+    break;
+  case 'rei':
+    console.log("O rei pode andar uma casa, em qualquer direção");
+    break;
+  case 'cavalo':
+    console.log("O cavalo pode andar quatro casas dispostas num formato de L, caso haja outras outras peças no caminho ele pula por cima delas");
+    break;
+  case 'torre':
+    console.log("A torre não tem limitação de casas, mas só pode se mover vertical e horizontalmente");
+    break;
+  case 'bispo':
+    console.log("O bispo não tem limitação de casas, mas só pode se mover diagonalmente");
+    break;
+  case 'rainha':
+    console.log("A rainha não tem limitação de casas e pode se mover para todas as direções");
+    break
+}
