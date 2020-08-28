9e7f1f82aa435309c206fe72497ae7cb8e62cc49
Bruno L Nogueira <nugobruno@gmail.com>
2020-08-28 14:44:51 -0300

    Exercicio 7 | Transformar porcentagem em conceitos

--- exercises/4.1/exercicio_07.js
+++ exercises/4.1/exercicio_07.js
@@ -1,17 +1,17 @@
-let nota=10;
+let grade = 100;
 
-if (nota*10<50) {
-  console.log("Sua nota é F");
-} else if (nota*10<=50) {
-  console.log("Sua nota é E");
-} else if (nota*10>=60) {
-  console.log("Sua nota é D");
-} else if (nota*10>=70) {
-  console.log("Sua nota é C");
-} else if (nota*10>=80) {
-  console.log("Sua nota é B");
-} else if (nota*10>=90) {
-  console.log("Sua nota é A");
+if (grade < 0 || grade > 100) {
+  console.log("Erro, nota incorreta!");
+} else if (grade >= 90) {
+  console.log("A");
+} else if (grade >= 80) {
+  console.log("B");
+} else if (grade >= 70) {
+  console.log("C");
+} else if (grade >= 60) {
+  console.log("D");
+} else if (grade >= 50) {
+  console.log("E");
 } else {
-  console.log("Erro");
+  console.log("F");
 }
