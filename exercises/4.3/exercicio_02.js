58585c87ec5793916142f0a30cc145e9d1bfdd0d
Bruno L Nogueira <nugobruno@gmail.com>
2020-09-01 10:53:18 -0300

    Exercicio 2 | Triangulo retangulo de asteriscos

---
+++ exercises/4.3/exercicio_02.js
@@ -0,0 +1,9 @@
+let n = 5;
+let square = "";
+
+if (n > 1) {
+  for (w = 0; w < n; w += 1) { //definindo que largura (w) = n//
+    square += "*"; //largura do quadrado = "*" n vezes//
+    console.log(square);
+  }
+}
