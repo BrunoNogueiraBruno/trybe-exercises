5bbb267cd80ebed20c8c404fa17304a64b2231df
Bruno L Nogueira <nugobruno@gmail.com>
2020-09-11 05:18:19 -0300

    ajeitando sliders

--- my-shell-scripts/exercises/5.4/script.js
+++ my-shell-scripts/exercises/5.4/script.js
@@ -7,19 +7,48 @@ let themeButton = document.querySelector("#theme-button");
 
   header.appendChild(elementTheme);
 
-  let elementThemeBg = document.createElement("div");
-  elementThemeBg.className = "element-theme-bg";
-  elementTheme.appendChild(elementThemeBg);
-
-  let elementThemeFontSize = document.createElement("div");
-  elementThemeFontSize.className = "element-theme-font-size";
-  elementTheme.appendChild(elementThemeFontSize);
-
-  let elementThemeFontFamily = document.createElement("div");
-  elementThemeFontFamily.className = "element-theme-font-family";
-  elementTheme.appendChild(elementThemeFontFamily);
-
-  let elementThemeDisplay = document.createElement("div");
-  elementThemeDisplay.className = "element-theme-display";
-  elementTheme.appendChild(elementThemeDisplay);
+  elementThemeSliders(elementTheme);
+  elementThemeSquares(elementTheme);
 })
+
+function elementThemeSliders(elementTheme) {
+  let elementThemeSliders = document.createElement("div");
+  elementThemeSliders.className = "element-theme-sliders";
+  elementTheme.appendChild(elementThemeSliders);
+
+  // elementThemeSliders.innerHTML = "Cor de fundo";
+  // elementSlider(elementThemeSliders);
+  elementThemeSliders.innerHTML += "<br><br>Tamanho da letra";
+  elementSlider(elementThemeSliders);
+  let text = document.querySelector(".container");
+  text = elementThemeSliders.value;
+  
+  elementThemeSliders.oninput = function() {
+    console.log(this.value);
+  }
+}
+
+
+
+function elementSlider(where) {
+  let slider = document.createElement("input");
+  slider.setAttribute("id", "element-slider");
+  slider.setAttribute("type", "range");
+  slider.setAttribute("min", "10");
+  slider.setAttribute("max", "100");
+  slider.setAttribute("value", "60");
+  where.appendChild(slider);
+
+  return slider;
+}
+
+function elementThemeSquares(elementTheme) {
+  let elementThemeFontSquare = document.createElement("div");
+  elementThemeFontSquare.className = "element-theme-squares";
+  elementTheme.appendChild(elementThemeFontSquare);
+
+  let elementThemeDisplaySquare = document.createElement("div");
+  elementThemeDisplaySquare.className = "element-theme-squares";
+  elementTheme.appendChild(elementThemeDisplaySquare);
+
+}
