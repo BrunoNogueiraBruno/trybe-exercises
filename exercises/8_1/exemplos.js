39d86b540c099ad2dba5bdff1a9dcad086603c69
Bruno Nogueira <nugobruno@gmail.com>
2020-10-05 16:07:57 -0300

    Exemplos

---
+++ exercises/8_1/exemplos.js
@@ -0,0 +1,58 @@
+//Exemplo de Array.forEach
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const multipliesFor2 = (element) => {
+//   console.log(`${element} * 2: ${element * 2}`);
+// };
+
+// numbers.forEach(multipliesFor2);
+
+// //Exemplo de Array.find
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// const verifyEven = (number) => number % 2 === 0;
+
+// const isEven = numbers.find(verifyEven);
+
+// console.log(isEven); // 30
+
+// console.log(verifyEven(9)); // False
+// console.log(verifyEven(14)); // True
+
+// // Outra forma de ser realizada sem a necessidade de criar uma nova função.
+// const isEven2 = numbers.find((number) => number % 2 === 0);
+
+// console.log(isEven2); // 30
+
+// Exemplo de Array.Some
+// const listNames = ['xaria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+// const verifyFirstLetter = (letter, names) => {
+//   return names.some((name) => name[0] === letter);
+// };
+
+// console.log(verifyFirstLetter('J', listNames)); // true
+// console.log(verifyFirstLetter('x', listNames)); // false
+
+// Exemplo de Array.every
+// const notes = {
+//   portugues: 'Aprovado',
+//   matematica: 'Reprovado',
+//   ingles: 'Aprovado',
+// };
+
+// const verifyNotes = (studentNotes) => {
+//   return Object.values(studentNotes).every((note) => note === 'Aprovado');
+// };
+
+// console.log(verifyNotes(notes));
+
+// Exemplo de Array.sort
+const food = ['arroz', 'feijão', 'farofa', 'chocolate', 'doce de leite'];
+food.sort();
+console.log(food);
+// [ 'arroz', 'chocolate', 'doce de leite', 'farofa', 'feijão' ]
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){ return a - b });
+console.log(points); // [1, 5, 10, 25, 40, 100]
