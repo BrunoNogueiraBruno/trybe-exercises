68736234e88cbea0a26e88d7b04bf6e2e994e932
Bruno Nogueira <nugobruno@gmail.com>
2020-10-06 01:47:24 -0300

    Exercicio 1

--- exercises/8_1/exemplos.js
+++ exercises/8_1/exemplos.js
@@ -7,22 +7,22 @@
 
 // numbers.forEach(multipliesFor2);
 
-// //Exemplo de Array.find
-// const numbers = [19, 21, 30, 3, 45, 22, 15];
+//Exemplo de Array.find
+const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-// const verifyEven = (number) => number % 2 === 0;
+const verifyEven = (number) => number % 2 === 0;
 
-// const isEven = numbers.find(verifyEven);
+const isEven = numbers.find(verifyEven);
 
-// console.log(isEven); // 30
+console.log(isEven); // 30
 
-// console.log(verifyEven(9)); // False
-// console.log(verifyEven(14)); // True
+console.log(verifyEven(9)); // False
+console.log(verifyEven(14)); // True
 
-// // Outra forma de ser realizada sem a necessidade de criar uma nova função.
-// const isEven2 = numbers.find((number) => number % 2 === 0);
+// Outra forma de ser realizada sem a necessidade de criar uma nova função.
+const isEven2 = numbers.find((number) => number % 2 === 0);
 
-// console.log(isEven2); // 30
+console.log(isEven2); // 30
 
 // Exemplo de Array.Some
 // const listNames = ['xaria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
